64c93e5e1bea9ba5c9e309059292aff0961b453a
asreal089 <lexx_89@hotmail.com>
2020-06-03 19:49:27 -0300

    Correçao de soap server.

--- server/consultaSoap.js
+++ server/consultaSoap.js
@@ -1,17 +1,17 @@
 const soap = require('soap');
-var url = 'http://127.0.0.1:8000/wsdl?wsdl';
-/*
+const url = 'http://localhost:3030/bmicalculator?wsdl';
+
 soap.createClient(url, function (err, client) {
-	if (err) return console.log(err);
-	console.log(client);
-});
-*/
-soap.createClient(url, function (error, client) {
-	client.Add({ intA: 10, intB: 25 }, function (error, result) {
-		if (error) {
-			console.log(error);
-			return;
-		}
-		console.log(result);
-	});
+	if (err) console.error(err);
+	else {
+		client.calculateBMI({ weight: 80, height: 1.77 }, function (
+			err,
+			response
+		) {
+			if (err) console.error(err);
+			else {
+				console.log(response);
+			}
+		});
+	}
 });
--- server/soapserver.js
+++ server/soapserver.js
@@ -1,36 +1,22 @@
-var http = require('http');
-var express = require('express');
-var app = express();
-var soap = require('soap');
-var bodyParser = require('body-parser');
-var myService = {
-	MyService: {
-		MyPort: {
-			Add: function (args) {
-				return {
-					AddResponse: args.intA,
-				};
+const soap = require('soap');
+const express = require('express');
+const app = express();
+
+const service = {
+	BMI_Service: {
+		BMI_Port: {
+			calculateBMI(args) {
+				const BMI = args.weight / (args.height * args.height);
+				return { bmi: BMI };
 			},
 		},
 	},
 };
-
-var xml = require('fs').readFileSync('wsdl.wsdl', 'utf8');
-
-var app = express();
-//body parser middleware are supported (optional)
-app.use(
-	bodyParser.raw({
-		type: function () {
-			return true;
-		},
-		limit: '5mb',
-	})
-);
-app.listen(8000, function () {
-	//Note: /wsdl route will be handled by soap module
-	//and all other routes & middleware will continue to work
-	soap.listen(app, '/wsdl', myService, xml, function () {
-		console.log('server initialized');
-	});
+// xml data is extracted from wsdl file created
+const xml = require('fs').readFileSync('./bmicalculator.wsdl', 'utf8');
+//create an express server and pass it to a soap server
+const server = app.listen(3030, function () {
+	const host = '127.0.0.1';
+	const port = server.address().port;
 });
+soap.listen(server, '/bmicalculator', service, xml);
